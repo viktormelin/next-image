d932f4f3b561bfeffd99662cd90e5da2699dea4a
dough <viktor.melin@me.com>
2022-09-05 00:32:24 +0200

    feat: added login button to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Index from '../components/Index';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
@@ -12,7 +12,9 @@ const Home: NextPage = () => {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
-			<Index />
+			<Link href='/auth/signin'>
+				<a>Sign In</a>
+			</Link>
 		</div>
 	);
 };
